6270b9d07201cb28e0fa045b5570bc4d36d8f6b9
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-09-17 11:01:33 +0200

    master: reduce gradle plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,6 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.android.tools.build:gradle:3.5.0")
+    implementation("com.android.tools.build:gradle:3.4.1")
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
