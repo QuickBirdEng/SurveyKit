7499ae1764f72766785b1d9bd0addfebf1673986
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-11-02 14:50:29 +0100

    feature/add-missing-page-types: add DateAnswerFormat and TimeAnswerFormat pages

---
+++ survey/src/main/java/com.quickbirdstudios.surveykit/backend/views/question_parts/TimePickerPart.kt
@@ -0,0 +1,81 @@
+package com.quickbirdstudios.surveykit.backend.views.question_parts
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.TimePicker
+import com.quickbirdstudios.surveykit.SurveyTheme
+import com.quickbirdstudios.surveykit.backend.views.main_parts.StyleablePart
+
+internal class TimePickerPart @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : LinearLayout(context, attrs), StyleablePart {
+
+    //region Members
+
+    private val timePicker: TimePicker
+
+    //endregion
+
+    //region Public API
+
+    var selected: Selected
+        get() = timePicker.selected
+        set(selected) {
+            timePicker.selected = selected
+        }
+    //endregion
+
+    //region Overrides
+
+    override fun style(surveyTheme: SurveyTheme) {
+        // TODO add styling if there is any
+    }
+
+    //endregion
+
+    //region Subtypes
+
+    data class Selected(val hour: Int, val minute: Int)
+
+    //endregion
+
+    //region Private API
+
+    private var TimePicker.selected: Selected
+        get() {
+            return if (Build.VERSION.SDK_INT < 23) {
+                Selected(
+                    hour = this.currentHour,
+                    minute = this.currentMinute
+                )
+            } else {
+                Selected(
+                    hour = this.hour,
+                    minute = this.minute
+                )
+            }
+        }
+        set(selected) {
+            return if (Build.VERSION.SDK_INT < 23) {
+                this.currentHour = selected.hour
+                this.currentMinute = selected.minute
+            } else {
+                this.hour = selected.hour
+                this.minute = selected.minute
+            }
+        }
+
+
+    //endregion
+
+    init {
+        this.gravity = Gravity.CENTER
+
+        timePicker = TimePicker(context)
+        this.addView(timePicker)
+    }
+
+}
---
+++ survey/src/main/java/com.quickbirdstudios.surveykit/backend/views/questions/DatePickerQuestionView.kt
@@ -0,0 +1,61 @@
+package com.quickbirdstudios.surveykit.backend.views.questions
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.quickbirdstudios.surveykit.AnswerFormat
+import com.quickbirdstudios.surveykit.StepIdentifier
+import com.quickbirdstudios.surveykit.backend.views.question_parts.DatePickerPart
+import com.quickbirdstudios.surveykit.backend.views.step.QuestionView
+import com.quickbirdstudios.surveykit.result.QuestionResult
+import com.quickbirdstudios.surveykit.result.question_results.DateQuestionResult
+
+internal class DatePickerQuestionView(
+    context: Context,
+    id: StepIdentifier,
+    isOptional: Boolean,
+    @StringRes title: Int?,
+    @StringRes text: Int?,
+    @StringRes nextButtonText: Int,
+    private val answerFormat: AnswerFormat.DateAnswerFormat,
+    private val preselected: AnswerFormat.DateAnswerFormat.Date?
+) : QuestionView(context, id, isOptional, title, text, nextButtonText) {
+
+    //region Members
+
+    private lateinit var datePicker: DatePickerPart
+
+    //endregion
+
+    //region Overrides
+
+    override fun createResults(): QuestionResult = DateQuestionResult(
+        id = id,
+        startDate = startDate,
+        stringIdentifier = datePicker.selected.toDate().toString(),
+        answer = datePicker.selected.toDate()
+    )
+
+    override fun isValidInput(): Boolean = true
+
+    override fun setState() {}
+    override fun setupViews() {
+        super.setupViews()
+
+        datePicker = content.add(DatePickerPart(context))
+        answerFormat.defaultValue?.let { datePicker.selected = it.toSelected() }
+        preselected?.let { datePicker.selected = it.toSelected() }
+    }
+
+    //endregion
+
+    //region Private API
+
+    private fun AnswerFormat.DateAnswerFormat.Date.toSelected(): DatePickerPart.Selected =
+        DatePickerPart.Selected(day = this.day, month = this.month, year = this.year)
+
+    private fun DatePickerPart.Selected.toDate(): AnswerFormat.DateAnswerFormat.Date =
+        AnswerFormat.DateAnswerFormat.Date(day = this.day, month = this.month, year = this.year)
+
+    //endregion
+
+}
