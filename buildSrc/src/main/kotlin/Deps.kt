3b16e4403ef203275e61c93905c77eed9decd0ee
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-11-23 11:23:39 -0800

    feature/espresso-tests: tmp

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,8 +1,9 @@
 object Versions {
     const val kotlin = "1.3.50"
     const val coroutines = "1.3.0"
-    const val junit = "4.12"
+    const val test = "1.1.1"
     const val lottie = "3.0.7"
+    const val espresso = "3.1.0"
 
     object AndroidSupport {
         const val appCompat = "1.0.0"
@@ -21,19 +22,28 @@ object Deps {
     object Kotlin {
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
-        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-        const val androidCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+        const val coroutines =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+        const val androidCoroutines =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
     }
 
     object AndroidSupport {
         const val appCompat = "androidx.appcompat:appcompat:${Versions.AndroidSupport.appCompat}"
-        const val constraint = "androidx.constraintlayout:constraintlayout:${Versions.AndroidSupport.constraintLayout}"
+        const val constraint =
+            "androidx.constraintlayout:constraintlayout:${Versions.AndroidSupport.constraintLayout}"
         const val recyclerView =
             "androidx.recyclerview:recyclerview:${Versions.AndroidSupport.recyclerView}"
     }
 
     object Test {
-        const val junit = "junit:junit:${Versions.junit}"
+        const val junit = "junit:junit:${Versions.test}"
+        const val core = "androidx.test:core:${Versions.test}"
+        const val runner = "androidx.test:runner:${Versions.test}"
+        const val rules = "androidx.test:rules:${Versions.test}"
+        const val junitExt = "androidx.test.ext:junit:${Versions.test}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+        const val espressoIntents = "androidx.test.espresso:espresso-intents:${Versions.espresso}"
     }
 
     const val lottie = "com.airbnb.android:lottie:${Versions.lottie}"
