c67381a614da64fc67692231798af34e889eb670
Klaus Niedermair <klaus.niedermair@quickbirdstudios.com>
2019-11-30 17:40:53 -0800

    feature/espresso-tests: Library version 0.2.0

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -1,7 +1,7 @@
 object Library {
     const val groupId = "com.quickbirdstudios"
     const val artifactId = "surveykit"
-    const val version = "0.1.0"
+    const val version = "0.2.0"
 
 
     object Meta {
